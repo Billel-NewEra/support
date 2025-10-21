e76972d84d43d47408683f7e0050dccaea7e7a54
Bil <biliro014@gmail.com>
2025-10-21 08:07:45 +0100

    ajout bouton

--- app.py
+++ app.py
@@ -431,7 +431,7 @@ def resend_confirmation(id):
             return "Email invalide", 400
 
     except Exception as e:
-        print(f"❌ Erreur envoi confirmation : {e}")
+        #print(f"❌ Erreur envoi confirmation : {e}")
         return str(e), 500
 
 
