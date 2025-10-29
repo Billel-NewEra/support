f1a95314e9b272ab1def4d49b1726b9093a375d5
Bil <biliro014@gmail.com>
2025-10-29 17:39:09 +0100

    message_personalise

--- app.py
+++ app.py
@@ -500,7 +500,7 @@ def send_planif_confirmation(id):
       <!-- Message principal -->
       <p>Bonjour <strong>{contact_nom}</strong>,</p>
 
-      <p>Votre demande de support a bien été planifiée par notre équipe technique.</p>
+      <p>Votre demande a bien été planifiée par notre équipe technique.</p>
 
       <p style="line-height:1.6;">
         <strong>Numéro de demande :</strong> {intervention_numero}<br>
